3377a73c352fed3d1ac9e77d4f1b6402d065bd38
XolaniG <34442183+XolaniGqamana@users.noreply.github.com>
2025-11-16 11:52:36 +0200

    Add files via upload

---
+++ revert.js
@@ -0,0 +1,8 @@
+let isRaining = true;
+
+if(isRaining) {
+    console.log("** Taking my umbrella when I need to go outside **");
+    }
+    else {
+        console.log("** I can leave my umbrella at home **");
+    }
